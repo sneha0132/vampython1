59f2040135052ce8ca46102dbd1e5a3a3ae9d91d
sneha0132 <snehasinghal125@gmail.com>
2024-09-25 16:09:54 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,32 @@
+#while loop is used to perform iteration until condition will false
+
+#print the no from 1 to 10 using while loop
+# x=1
+
+# while x<11:
+#     print(x)
+#     x= x+1
+
+# #WAP to print the no from 10 to 1 using while loop
+# y = 10
+# while y>0:
+#     print(y)
+#     y=y-1
+
+#WAP to print the given pattern using while loop 1 3 5 9 11
+# x = 1
+# while  x<12: 
+#     if x == 7:
+#         print()
+#     else:
+#         print(x)
+#     x=x+2
+
+# or
+x=1
+while x<12:
+    if x!=7:
+        print(x)
+        x= x+2
+        
+
