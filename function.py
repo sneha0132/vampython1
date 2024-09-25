70faaa51083e3f1a01d48f1bac5b9f16247f406b
sneha0132 <snehasinghal125@gmail.com>
2024-09-25 16:12:31 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,20 @@
+#function can perform any task and it can reuse anything
+# def printName():
+#     print("My function name is print")
+
+# #function calling
+# printName()
+
+#argument function and parameter function
+# def myAge(age):
+#     print("my age is", age)
+
+#     myAge(21)
+
+#find the area of square using function
+area = int(input("Enter the square side"))
+           
+def area(a):
+    return a*a
+    output = area(area)
+    print("Area is", output)
